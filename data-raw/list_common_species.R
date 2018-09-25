65b9509d63cceabe35aedfcd742282a0345ca8ac
Andy Kleinhesselink <andy.kleinhesselink@gmail.com>
2018-09-24 17:51:56 -0700

    Modify script to list common species
    list species by max cover and total number of plots where it occurs

--- data-raw/list_common_species.R
+++ data-raw/list_common_species.R
@@ -1,37 +1,13 @@
-library(dplyr, tidyr)
-library(ggplot2)
 rm(list = ls())
+library(tidyverse)
 
-load('~/Dropbox/Sedgwick_cover_data/cover.data')
+outfile <- 'data-raw/species_rarity.csv'
 
-max_cover <- cover %>% 
-  mutate( total = ifelse(is.na(total), 0 , total )) %>%
-  group_by( species) %>% 
-  summarise ( mx = max(total) ) %>% arrange( mx )
+load('data/plot_cover.rda')
 
-commonness <-  merge(plot_counts, max_cover) 
+max_cover <- apply( plot_cover[, -1], 2, max) 
+total_plots <- apply( plot_cover[, -1], 2, function(x) sum( x > 0) )
 
-commonness <- commonness %>% arrange( desc(mx), desc(nplots))
+species_commoness <- data.frame( cbind( max_cover, total_plots) )
 
-write.csv(file = '~/Dropbox/Sedgwick_cover_data/species_commonness.csv', commonness,  row.names = F)
-
-max_cover <- 
-  cover %>% 
-  group_by( species, plot ) %>% 
-  filter( sum(nsubplots) == max(sum(nsubplots))) %>% 
-  summarise(avg_cover = mean(total)) %>% 
-  arrange( species, desc(avg_cover)) %>% 
-  filter( row_number() < 3)
-
-indicator_sp <- 
-  cover %>% 
-  group_by( species, plot ) %>% 
-  filter( (species == 'Plantago erecta' & total > 0) | (species == 'Croton setigerus' & total > 0)) %>% 
-  summarise( avg_cover = mean(total))
-
-spp_phenology <- rbind(max_cover, indicator_sp) %>% select( species, plot, avg_cover) %>% distinct() 
-
-write.csv(spp_phenology, '~/Dropbox/Sedgwick_cover_data/2018_phenology_sites.csv', row.names = F)
-
-hts<- read.csv('~/Dropbox/2017-traits/2017-plant-heights.csv')
-hts %>% group_by(species) %>% summarise(n())
+write_csv(species_commoness, path = outfile)
